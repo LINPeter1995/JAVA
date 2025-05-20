b868473c0bd717b35bf7459bd80390973ef2345e
LINPeter1995 <liyanglin08@gmail.com>
2025-05-20 15:56:20 +0800

    Ex02_01

---
+++ Java基礎必修/src/ex02/Ex02_01.java
@@ -0,0 +1,12 @@
+package ex02;
+
+public class Ex02_01 {
+
+	public static void main(String[] args) {
+		System.out.println("十進制10->"+10);
+		System.out.println("二進制10->"+0b10);
+		System.out.println("八進制10->"+010);
+		System.out.println("十六進制10->"+0x10);
+	}
+
+}
