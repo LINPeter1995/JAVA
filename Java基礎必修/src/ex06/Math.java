4a30e0ff031ed4fa1e95e56459c5ff95edc2e3ca
LINPeter1995 <liyanglin08@gmail.com>
2025-05-21 15:31:01 +0800

    更多練習

---
+++ Java基礎必修/src/ex06/Math.java
@@ -0,0 +1,12 @@
+package ex06;
+
+public class Math {
+	static void mul(int x, int y) {
+		System.out.println(x + "*" + y + " = " + (x * y));
+	}
+
+	void div(int x, int y) {
+		System.out.println(x + "/" + y + " = " + (x / y));
+	}
+
+}
