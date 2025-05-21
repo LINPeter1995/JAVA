6a023df8e68ff4744f3b7577482e73d8a4b17f60
LINPeter1995 <liyanglin08@gmail.com>
2025-05-21 15:26:34 +0800

    更多練習

---
+++ Java基礎必修/src/ex13/Rate1.java
@@ -0,0 +1,30 @@
+package ex13;
+
+public class Rate1 {
+
+	public static void main(String[] args) {
+		Thread tortoise = new Thread() {
+			public void run() {
+				for (int i = 0; i < 20; i++) {
+					System.out.println("烏龜共跑: " + i + "公里");
+				}
+				System.out.println("烏龜到達終點");
+			}
+		};
+		Thread rabbit = new Thread() {
+			public void run() {
+				for (int i = 4; i <= 20; i += 4) {
+					if (Math.random() > 0.2) {
+						System.out.println("兔子休息");
+						i -= 4;
+					} else {
+						System.out.println("兔子共跑: " + i + "公里");
+					}
+				}
+				System.out.println("兔子到達終點");
+			}
+		};
+		rabbit.start();
+		tortoise.start();
+	}
+}
